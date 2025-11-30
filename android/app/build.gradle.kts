c39171f9b44b50475bd60e1465b6891301f0f360
Lubomir Mitana <lubom@mac-mini.mitana>
2025-11-30 20:08:50 +0100

    fix: update bundle identifiers from com.example to com.lubmir2k
    - Android: namespace and applicationId
    - iOS: PRODUCT_BUNDLE_IDENTIFIER in project.pbxproj
    - macOS: PRODUCT_BUNDLE_IDENTIFIER in AppInfo.xcconfig and project.pbxproj
    - Move MainActivity.kt to correct package path

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.habit_tracker"
+    namespace = "com.lubmir2k.habit_tracker"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.habit_tracker"
+        applicationId = "com.lubmir2k.habit_tracker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
